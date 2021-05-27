e1a1a031061199f6a6bd7afc3ff96ef9b7ea1307
DKaviya <dinesh.mnr@gmail.com>
2021-05-27 12:14:20 +0530

    added logs

--- src/main/java/com/example/service2/Service2Controller.java
+++ src/main/java/com/example/service2/Service2Controller.java
@@ -1,14 +1,24 @@
 package com.example.service2;
 
+import org.apache.logging.log4j.LogManager;
+import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class Service2Controller {
 	
+	private static final Logger LOG = (Logger) LogManager.getLogger(Service2Controller.class);
+	
 	@GetMapping
 	public String welcome() {
+		LOG.info("Log dependency ===> org.apache.logging.log4j.LogManager");
+		LOG.info("Logger INFO Statement");
+		LOG.debug("Logger DEBUG Statement");
+		LOG.error("Logger ERROR Statement");
 		return "Welcome to Service 2";
+		
+		
 	}
 
 }
