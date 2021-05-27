b3979073b2a873beea50e715a58386e8179b34db
DKaviya <dinesh.mnr@gmail.com>
2021-05-27 07:41:17 +0530

    first commit

---
+++ src/main/java/com/example/service2/Service2Controller.java
@@ -0,0 +1,14 @@
+package com.example.service2;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Service2Controller {
+	
+	@GetMapping
+	public String welcome() {
+		return "Welcome to Service 2";
+	}
+
+}
