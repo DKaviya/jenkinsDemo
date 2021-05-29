67811a709355754d5b5f76d6e9d597bd151faa68
DKaviya <dinesh.mnr@gmail.com>
2021-05-29 14:52:29 +0530

    log4j included

--- src/main/java/com/example/service2/Service2Controller.java
+++ src/main/java/com/example/service2/Service2Controller.java
@@ -1,16 +1,17 @@
 package com.example.service2;
 
 import org.apache.logging.log4j.LogManager;
-import org.slf4j.Logger;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@RestController()
+@RequestMapping("/pipeline")
 public class Service2Controller {
 	
-	private static final Logger LOG = (Logger) LogManager.getLogger(Service2Controller.class);
+	private static final org.apache.logging.log4j.Logger LOG = LogManager.getLogger(Service2Controller.class);
 	
-	@GetMapping
+	@GetMapping("/demo")
 	public String welcome() {
 		LOG.info("Log dependency ===> org.apache.logging.log4j.LogManager");
 		LOG.info("Logger INFO Statement");
